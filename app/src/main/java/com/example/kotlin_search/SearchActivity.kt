9ec5703529901d424985bea655c84adc1caeaa3b
Aziz <techfort20@gmail.com>
2020-07-19 02:27:56 +0600

    tag

--- app/src/main/java/com/example/kotlin_search/SearchActivity.kt
+++ app/src/main/java/com/example/kotlin_search/SearchActivity.kt
@@ -5,19 +5,22 @@ import android.database.Cursor
 import android.database.MatrixCursor
 import android.os.Bundle
 import android.provider.BaseColumns
-import android.widget.CursorAdapter
-import android.widget.SearchView
-import android.widget.SimpleCursorAdapter
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.nex3z.flowlayout.FlowLayout
+
 
 class SearchActivity : AppCompatActivity() {
     lateinit var searchView: SearchView
+    lateinit var inflater:LayoutInflater
+    lateinit var flowLayout : FlowLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_view)
         searchView = findViewById(R.id.search_view)
-
+        flowLayout = findViewById(R.id.flow_layout)
+        inflater = LayoutInflater.from(this)
 
         val from = arrayOf(SearchManager.SUGGEST_COLUMN_TEXT_1)
         val to = intArrayOf(R.id.item_label)
@@ -29,7 +32,16 @@ class SearchActivity : AppCompatActivity() {
             to,
             CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
         )
-        val suggestions = listOf("Apple", "Apply","Application","Alcohol","Blueberry","Blue", "Carrot", "Daikon")
+        val suggestions = listOf(
+            "Apple",
+            "Apply",
+            "Application",
+            "Alcohol",
+            "Blueberry",
+            "Blue",
+            "Carrot",
+            "Daikon"
+        )
 
         searchView.suggestionsAdapter = cursorAdapter
 
@@ -42,8 +54,14 @@ class SearchActivity : AppCompatActivity() {
 
             override fun onQueryTextChange(query: String?): Boolean {
 
-                val cursor = MatrixCursor(arrayOf(BaseColumns._ID, SearchManager.SUGGEST_COLUMN_TEXT_1))
+                val cursor =
+                    MatrixCursor(arrayOf(BaseColumns._ID, SearchManager.SUGGEST_COLUMN_TEXT_1))
                 query?.let {
+                    if(it.contains(" ")) {
+                        addTage(it.trim())
+                        searchView.setQuery("", false);
+                        searchView.clearFocus();
+                    }
                     suggestions.forEachIndexed { index, suggestion ->
                         if (suggestion.contains(query, true))
                             cursor.addRow(arrayOf(index, suggestion))
@@ -57,6 +75,11 @@ class SearchActivity : AppCompatActivity() {
 
         searchView.setOnSuggestionListener(object : SearchView.OnSuggestionListener {
             override fun onSuggestionSelect(position: Int): Boolean {
+                Toast.makeText(
+                    applicationContext,
+                    "Select ${position.toString()}",
+                    Toast.LENGTH_LONG
+                ).show()
                 return false
             }
 
@@ -67,9 +90,19 @@ class SearchActivity : AppCompatActivity() {
                 searchView.setQuery(selection, false)
 
                 Toast.makeText(applicationContext, selection, Toast.LENGTH_LONG).show()
+                addTage(selection)
+                searchView.setQuery("", false)
+                searchView.clearFocus()
                 return true
             }
 
         })
+    }
+
+    fun addTage(tage: String){
+        val view  = inflater.inflate(R.layout.tag_layout, flowLayout, false) as LinearLayout
+        val textView = view.getChildAt(0) as TextView
+        textView.text = tage
+        flowLayout.addView(view)
     }
 }
